7be0f75e697fd40fd6276aea6c6fc6c7d9fe0edb
Luke <hello@lukeonthebrightside.com>
2016-08-21 00:07:18 +0800

    added player's turn announcement

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
-console.log('Hello frontend');
+var grids = document.getElementsByClassName('grid');
+
+var turn = 0;
+
+
+console.log (grids);
+
+for(var i=0;i<grids.length;i++){
+  grids[i].addEventListener('click',checkcell,false);
+}
+
+function checkturn(){
+  turn++;
+  checkcell;
+}
+
+function checkcell(){
+  if( (turn%2) ===0 ) {
+    console.log(this);
+    this.style.backgroundColor='blue';
+    document.querySelector('.player-announcement').textContent = "Player's 2 turn";
+    turn++;
+  } else {
+    console.log(this);
+    this.style.backgroundColor='green';
+    document.querySelector('.player-announcement').textContent = "Player's 1 turn";
+    turn++;
+  }
+}
