f5adf23d7fe0e809c7c1e810cc73cdc631158ca7
Luke <hello@lukeonthebrightside.com>
2016-08-21 23:17:04 +0800

    included checkwinner() function

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 var grids = document.getElementsByClassName('grid');
 var turn = 0;
-player1moves=[];
-player2moves=[];
-occupiedCell =[];
-
-console.log (grids);
+// player1moves=[];
+// player2moves=[];
+// occupiedCell =[];
+// console.log (grids);
 
 for(var i=0;i<grids.length;i++){
-  grids[i].addEventListener('click',checkcell,false);
+    grids[i].addEventListener('click',checkcell,false);
 }
 
 function checkturn(){
@@ -17,24 +16,61 @@ function checkturn(){
 
 function checkcell(){
   if( (turn%2) ===0 ) {
-    console.log(this.id);
-    this.style.backgroundColor='blue';
-    player1moves.push(this.id);
-    console.log(indexOf(this));
-    (occupiedCell.push(this.id)).sort;
-    console.log(occupiedCell);
-    document.querySelector('.player-announcement').textContent = "Player's 2 turn";
-    turn++;
+    this.className = 'grid turnBlue';
+    this.value = 'x';
+      if (!checkWinner(grids)){
+          document.querySelector('.player-announcement').textContent = "Player's 2 turn";
+          turn++;
+      } else {
+          document.querySelector('.player-announcement').textContent = "Player 1 Wins!";
+      }
   } else {
-    console.log(this.id);
-    this.style.backgroundColor='green';
-    player2moves.push(this.id);
-    occupiedCell.push(this.id);
-    document.querySelector('.player-announcement').textContent = "Player's 1 turn";
-    turn++;
+    this.className = 'grid turnGreen';
+    this.value = 'o';
+      if (!checkWinner(grids)){
+          document.querySelector('.player-announcement').textContent = "Player's 1 turn";
+          turn++;
+      } else {
+          document.querySelector('.player-announcement').textContent = "Player 2 Wins!";
+      }
   }
 }
 
-function checkwinner (){
-
+function checkWinner(array) {
+  if (checkRow(array) || checkCol(array) || checkDiagonal(array)){
+    window.alert('We have a winner!');
+    return true;
+  } else return false;
 }
+
+function checkRow(array){
+  if ((array[0].value == array[1].value && array[0].value == array[2].value && array[0].value != undefined) ||
+      (array[3].value == array[4].value && array[3].value == array[5].value && array[3].value != undefined) ||
+      (array[6].value == array[7].value && array[6].value == array[8].value && array[6].value != undefined) ){
+        return true;
+      } else return false;
+}
+
+function checkCol(array){
+  if ((array[0].value == array[3].value && array[0].value == array[6].value && array[0].value != undefined) ||
+      (array[1].value == array[4].value && array[1].value == array[7].value && array[1].value != undefined) ||
+      (array[2].value == array[8].value && array[2].value == array[8].value && array[2].value != undefined) ){
+        return true;
+      } else return false;
+}
+
+function checkDiagonal(array){
+  if ((array[0].value == array[4].value && array[0].value == array[8].value && array[0].value != undefined) ||
+      (array[6].value == array[4].value && array[6].value == array[2].value && array[6].value != undefined) ){
+        return true;
+      } else return false;
+}
+
+// function findWithAttr(array, attr, value) {
+//     for(var i = 0; i < array.length; i += 1) {
+//         if(array[i][attr] === value) {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
