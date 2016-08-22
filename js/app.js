ebd5d96cf659c1b392bc53bf5cf216d555491985
Luke <hello@lukeonthebrightside.com>
2016-08-22 10:13:10 +0800

    amendments

--- js/app.js
+++ js/app.js
@@ -1,17 +1,21 @@
 var grids = document.getElementsByClassName('grid');
 var turn = 0;
-// player1moves=[];
-// player2moves=[];
-// occupiedCell =[];
-// console.log (grids);
 
-for(var i=0;i<grids.length;i++){
-    grids[i].addEventListener('click',checkcell,false);
-}
 
-function checkturn(){
-  turn++;
-  checkcell;
+console.log(typeof(grids));
+var arr = Array.from(grids);
+console.log(arr);
+arr.splice(3,1);
+console.log(grids);
+
+listentoclicks();
+
+function listentoclicks(){
+  for(var i=0;i<arr.length;i++){
+    if (grids[i].value == undefined) {
+        grids[i].addEventListener('click',checkcell,false);
+    }
+  }
 }
 
 function checkcell(){
@@ -21,6 +25,7 @@ function checkcell(){
       if (!checkWinner(grids)){
           document.querySelector('.player-announcement').textContent = "Player's 2 turn";
           turn++;
+          listentoclicks();
       } else {
           document.querySelector('.player-announcement').textContent = "Player 1 Wins!";
       }
@@ -30,6 +35,7 @@ function checkcell(){
       if (!checkWinner(grids)){
           document.querySelector('.player-announcement').textContent = "Player's 1 turn";
           turn++;
+          listentoclicks();
       } else {
           document.querySelector('.player-announcement').textContent = "Player 2 Wins!";
       }
@@ -54,7 +60,7 @@ function checkRow(array){
 function checkCol(array){
   if ((array[0].value == array[3].value && array[0].value == array[6].value && array[0].value != undefined) ||
       (array[1].value == array[4].value && array[1].value == array[7].value && array[1].value != undefined) ||
-      (array[2].value == array[8].value && array[2].value == array[8].value && array[2].value != undefined) ){
+      (array[2].value == array[5].value && array[2].value == array[8].value && array[2].value != undefined) ){
         return true;
       } else return false;
 }
